7aa1c65680ae4d9c1d53722028697e7a16df3737
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2021-06-26 20:20:23 -0600

    adds auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/login/jwtvalidate", to: "users#jwt_validate"
+  resource :user, only: [:create]
+  post "/login", to: "user#login"
+  get "/login/validate", to: "user#jwt_validate"
 end
