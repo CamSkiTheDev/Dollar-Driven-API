08f36f3082b9966965cac1f6942a82681533f322
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2021-06-26 20:20:11 -0600

    adds user controller and model

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,34 @@
+class UserController < ApplicationController
+    before_action :authorized, only: [:jwt_validate]
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+    def login
+        @user = User.find_by(email: params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+    def jwt_validate
+        render json: @user
+    end
+
+    private
+
+    def user_params
+        params.permit(:full_name, :email, :password)
+    end
+end
