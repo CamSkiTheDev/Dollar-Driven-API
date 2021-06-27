bfc7f7361102a4c4b223155994a35cbdd9c15517
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2021-06-27 13:43:39 -0600

    adds jwt auth, user controller, model and auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/login/validate", to: "users#validate_jwt"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(full_name: "Cameron Lucas", email: "cameronlucasaim@gmail.com", password: "Password1")
